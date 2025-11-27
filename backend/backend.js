9a3ece9283d268f1fcb8fe2e223bef5cdb3c6774
Nicolas Fernandes <nicolaschfernandes@gmail.com>
2025-11-26 22:51:19 -0300

    feat: Correção de 'headers'

--- backend/backend.js
+++ backend/backend.js
@@ -6,10 +6,16 @@ const routerSort = require('./router/router.js');
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+    res.header('Access-Control-Allow-Methods', 'GET, POST');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.get('/v1', (req, res) => {
     res.send('Hello World');
 });
-
 app.use('/v1', routerSort);
 
 app.listen(port, () => {
